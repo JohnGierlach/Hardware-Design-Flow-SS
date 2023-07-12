07894e6d82b4726e0f366cb2b176912fdc3772a4
John Gierlach <jgierlach17@gmail.com>
2023-07-12 16:08:22 -0400

    TODO: Fix bugs with riscv_tb

--- rtl/riscv_tb.v
+++ rtl/riscv_tb.v
@@ -21,4 +21,27 @@
 
 
 module riscv_tb();
+    
+    parameter WIDTH = 32;
+    reg clk, rst;
+    reg[WIDTH-1:0] addr;
+    wire[4:0] rd;
+    
+    rsicv_top #(WIDTH) UUT(.clk(clk), .rst(rst), .addr(addr), .rd(rd));
+    
+    integer i; 
+    
+    initial
+    begin
+        clk = 0;
+        rst = 1;
+        #20;
+        rst = 0;
+        addr = 32'h00730033;
+        #20
+        addr = 32'h00000000;
+        
+    end
+    
+    always #20 clk = ~clk;
 endmodule
